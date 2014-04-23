e0076790267cd452b315a5223b899e9f25c9c8b5
Marius Cristea <cristea.marius.alexandru@gmail.com>
2014-04-23 14:36:59 -0500

    Merge development with production

---
+++ latest/javascript/admin-review.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function(){
+	
+	jQuery('input:radio[name="cwp_meta_box_check"]').change(function(){
+
+    if(jQuery(this).is(':checked')){
+       jQuery(".review-settings-notice").show();
+       jQuery(".review-settings-group").show();
+       jQuery(".isReviewYes").show();
+       
+    }
+});
+
+
+})
