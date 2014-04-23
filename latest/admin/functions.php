2db4a0eaeaa206b1f167af29723e5f95ecf75c46
mariusalex20 <cristea.marius.alexandru@gmail.com>
2014-04-23 22:36:06 +0300

    test

--- latest/admin/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-		require(  plugin_dir_path (   __FILE__ ) . 'inc/config.php' ); 
-		require(  plugin_dir_path (   __FILE__) . 'inc/adminOptionsValidator.php' );
-		require(  plugin_dir_path (   __FILE__ ) . 'inc/adminSanitizer.php' );
-		require(  plugin_dir_path (   __FILE__ ) . 'inc/render.php' ); 
-		require(  plugin_dir_path ( __FILE__ ) . 'inc/loader.php' );  
-		
-?>
