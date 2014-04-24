f9152212ea9072b665d8738164bcadbf20408e05
Marius Cristea <cristea.marius.alexandru@gmail.com>
2014-04-24 10:41:02 -0500

    Merge development with production from 1.1.7.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0 to .W.P.

--- latest/admin/inc/config.php
+++ latest/admin/inc/config.php
@@ -1,208 +1,236 @@
-<?php
-	class cwpposConfig{
-		public static $admin_page_menu_name ;
-		public static  $admin_page_title 	;
-		public static  $admin_page_header 	;
-		public static  $admin_template_directory ;
-		public static  $admin_template_directory_uri ;
-		public static  $admin_uri 	;
-		public static $admin_path  ;
-		public static  $menu_slug 	;
-		public static $structure;
-		public static $review_categories_array;
-		public static $categories_array;
-		public static $shortname;
-		public static $all_review_categories_array;
-		public static $all_categories_array;
-		public static $categories_ids;
-
-		public static function init(){
-			self::$admin_page_menu_name 	 = "WP Product Review Options";
-			self::$admin_page_title 		 = "WP Product Review Options";
-			self::$admin_page_header	 	 = "WP Product Review Options";
-			self::$shortname 			     = "cwppos";
-			self::$admin_template_directory_uri  =  plugins_url (  'wp-product-review/admin/layout' );
-			self::$admin_template_directory  =  plugins_url ('wp-product-review/admin/layout' );
-			self::$admin_uri  		= 	 plugins_url (   'wp-product-review/admin/' ); 
-			self::$admin_path 	 	= 	 plugins_url ( 'wp-product-review/admin/');
-			self::$menu_slug  		= 	"cwppos_options";
-			self::$all_categories_array = array();
-			self::$all_review_categories_array = array();
-			self::$categories_array = array(); 
- 
-			self::$structure	= array(
-						array(
-							 "type"=>"tab",
-							 "name"=>"Rating colors",
-							 "options"=>array(
-								
-								array(
-									"type"=>"title",
-									"name"=>"Rating Colors"
-								) ,
- 
-								array(	
-									"type"=>"color",
-									"name"=>"Rating default color",
-									"description"=>"Select the color to be used by default on rating.",
-									"id"=>"cwppos_rating_default",
-									"default"=>"#E1E2E0"
-								),
-								array(	
-									"type"=>"color",
-									"name"=>"Weak rating",
-									"description"=>"Select the color to be used when the rating is weak. ( < 2.5)",
-									"id"=>"cwppos_rating_weak",
-									"default"=>"#FF7F66"
-								),
- 
-								array(	
-									"type"=>"color",
-									"name"=>"Not bad rating",
-									"description"=>"Select the color to be used when the rating is not bad. ( > 2.5 and < 5)",
-									"id"=>"cwppos_rating_notbad",
-									"default"=>"#FFCE55"
-								),
- 
-								array(	
-									"type"=>"color",
-									"name"=>"Good rating",
-									"description"=>"Select the color to be used when the rating is good. ( >5 and <7.5)",
-									"id"=>"cwppos_rating_good",
-									"default"=>"#50C1E9"
-								),
- 
-								array(	
-									"type"=>"color",
-									"name"=>"Very good rating",
-									"description"=>"Select the color to be used when the rating is very good. ( 7.5 < and <10)",
-									"id"=>"cwppos_rating_very_good",
-									"default"=>"#8DC153"
-								)
-							
-							 )
-						) ,
-						array(
-							 "type"=>"tab",
-							 "name"=>"Typography",
-							 "options"=>array(
-								
-								array(
-									"type"=>"title",
-									"name"=>"Typography options"
-								) ,
- 
-								array(	
-									"type"=>"color",
-									"name"=>"Font color",
-									"description"=>"Select the color to be used on the font.",
-									"id"=>"cwppos_font_color",
-									"default"=>"#3D3D3D"
-								),
-								array(	
-									"type"=>"color",
-									"name"=>"'Pros' text color",
-									"description"=>"Select the color to be used on the 'Pros' text.",
-									"id"=>"cwppos_pros_color",
-									"default"=>"#8DC153"
-								),
-								array(	
-									"type"=>"color",
-									"name"=>"'Cons' text color",
-									"description"=>"Select the color to be used on the 'Cons' text.",
-									"id"=>"cwppos_cons_color",
-									"default"=>"#C15353"
-								),
-  
- 
-								
-								array(
-								 "type"=>"input_text",
-								 "name"=>"Pros text",							 
-								 "description"=>"Specify text for pros heading",
-								 "id"=>"cwppos_pros_text",
-								 "default"=>"Pros"
-							   ),
- 
-								
-								array(
-								 "type"=>"input_text",
-								 "name"=>"Cons text",							 
-								 "description"=>"Specify text for cons heading",
-								 "id"=>"cwppos_cons_text",
-								 "default"=>"Cons"
-							   ) 
-							
-							 )
-						) ,
-						array(
-							 "type"=>"tab",
-							 "name"=>"Buy button",
-							 "options"=>array(
-								
-								array(
-									"type"=>"title",
-									"name"=>"Buy button options"
-								) ,
-								array(
-									"type"=>"select",
-									"name"=>"Show button icon",
-									"description"=>"Show icon on the cart icon on button.",
-									"id"=>"cwppos_show_icon",
-									"options"=>array("yes"=>"Yes","no"=>"No"),
-									"default"=>"yes"
-								),
-								array(	
-									"type"=>"color",
-									"name"=>"Button border",
-									"description"=>"Select the border color to be used on the buy button for the default state",
-									"id"=>"cwppos_buttonbd_color",
-									"default"=>"#3BAEDA"
-								),
-								array(	
-									"type"=>"color",
-									"name"=>"Button border hover",
-									"description"=>"Select the border color to be used on the buy button for the hover state",
-									"id"=>"cwppos_buttonbh_color",
-									"default"=>"#3BAEDA"
-								),
-								array(	
-									"type"=>"color",
-									"name"=>"Button background",
-									"description"=>"Select the background color to be used on the buy button for the default state",
-									"id"=>"cwppos_buttonbkd_color",
-									"default"=>"#ffffff"
-								),
-								array(	
-									"type"=>"color",
-									"name"=>"Button background hover",
-									"description"=>"Select the background color to be used on the buy button for the hover  state",
-									"id"=>"cwppos_buttonbkh_color",
-									"default"=>"#3BAEDA"
-								),
-								array(	
-									"type"=>"color",
-									"name"=>"Button text color",
-									"description"=>"Select the text color to be used on the buy button for the default state",
-									"id"=>"cwppos_buttontxtd_color",
-									"default"=>"#3BAEDA"
-								),
-								array(	
-									"type"=>"color",
-									"name"=>"Button text color hover",
-									"description"=>"Select the text color to be used on the buy button for the hover state",
-									"id"=>"cwppos_buttontxth_color",
-									"default"=>"#FFFFFF"
-								) 
-							
-							 )
-						) 
-
-		
-					);
-
-
-			 
-		}	 
-	
-	} 
+<?php
+	class cwpposConfig{
+		public static $admin_page_menu_name ;
+		public static  $admin_page_title 	;
+		public static  $admin_page_header 	;
+		public static  $admin_template_directory ;
+		public static  $admin_template_directory_uri ;
+		public static  $admin_uri 	;
+		public static $admin_path  ;
+		public static  $menu_slug 	;
+		public static $structure;
+		public static $review_categories_array;
+		public static $categories_array;
+		public static $shortname;
+		public static $all_review_categories_array;
+		public static $all_categories_array;
+		public static $categories_ids;
+
+		public static function init(){
+			self::$admin_page_menu_name 	 = "WP Product Review Options";
+			self::$admin_page_title 		 = "WP Product Review Options";
+			self::$admin_page_header	 	 = "WP Product Review Options";
+			self::$shortname 			     = "cwppos";
+			self::$admin_template_directory_uri  =  plugins_url (  'wp-product-review/admin/layout' );
+			self::$admin_template_directory  =  plugins_url ('wp-product-review/admin/layout' );
+			self::$admin_uri  		= 	 plugins_url (   'wp-product-review/admin/' ); 
+			self::$admin_path 	 	= 	 plugins_url ( 'wp-product-review/admin/');
+			self::$menu_slug  		= 	"cwppos_options";
+			self::$all_categories_array = array();
+			self::$all_review_categories_array = array();
+			self::$categories_array = array(); 
+ 
+			self::$structure	= array(
+						array(
+							 "type"=>"tab",
+							 "name"=>"Rating colors",
+							 "options"=>array(
+								
+								array(
+									"type"=>"title",
+									"name"=>"Rating Colors"
+								) ,
+ 
+								array(	
+									"type"=>"color",
+									"name"=>"Rating default color",
+									"description"=>"Select the color to be used by default on rating.",
+									"id"=>"cwppos_rating_default",
+									"default"=>"#E1E2E0"
+								),
+								array(	
+									"type"=>"color",
+									"name"=>"Weak rating",
+									"description"=>"Select the color to be used when the rating is weak. ( < 2.5)",
+									"id"=>"cwppos_rating_weak",
+									"default"=>"#FF7F66"
+								),
+ 
+								array(	
+									"type"=>"color",
+									"name"=>"Not bad rating",
+									"description"=>"Select the color to be used when the rating is not bad. ( > 2.5 and < 5)",
+									"id"=>"cwppos_rating_notbad",
+									"default"=>"#FFCE55"
+								),
+ 
+								array(	
+									"type"=>"color",
+									"name"=>"Good rating",
+									"description"=>"Select the color to be used when the rating is good. ( >5 and <7.5)",
+									"id"=>"cwppos_rating_good",
+									"default"=>"#50C1E9"
+								),
+ 
+								array(	
+									"type"=>"color",
+									"name"=>"Very good rating",
+									"description"=>"Select the color to be used when the rating is very good. ( 7.5 < and <10)",
+									"id"=>"cwppos_rating_very_good",
+									"default"=>"#8DC153"
+								)
+							
+							 )
+						) ,
+						array(
+							 "type"=>"tab",
+							 "name"=>"Typography",
+							 "options"=>array(
+								
+								array(
+									"type"=>"title",
+									"name"=>"Typography options"
+								) ,
+ 
+								array(	
+									"type"=>"color",
+									"name"=>"Font color",
+									"description"=>"Select the color to be used on the font.",
+									"id"=>"cwppos_font_color",
+									"default"=>"#3D3D3D"
+								),
+								array(	
+									"type"=>"color",
+									"name"=>"'Pros' text color",
+									"description"=>"Select the color to be used on the 'Pros' text.",
+									"id"=>"cwppos_pros_color",
+									"default"=>"#8DC153"
+								),
+								array(	
+									"type"=>"color",
+									"name"=>"'Cons' text color",
+									"description"=>"Select the color to be used on the 'Cons' text.",
+									"id"=>"cwppos_cons_color",
+									"default"=>"#C15353"
+								),
+  
+ 
+								
+								array(
+								 "type"=>"input_text",
+								 "name"=>"Pros text",							 
+								 "description"=>"Specify text for pros heading",
+								 "id"=>"cwppos_pros_text",
+								 "default"=>"Pros"
+							   ),
+ 
+								
+								array(
+								 "type"=>"input_text",
+								 "name"=>"Cons text",							 
+								 "description"=>"Specify text for cons heading",
+								 "id"=>"cwppos_cons_text",
+								 "default"=>"Cons"
+							   ) 
+							
+							 )
+						) ,
+						array(
+							 "type"=>"tab",
+							 "name"=>"Buy button",
+							 "options"=>array(
+								
+								array(
+									"type"=>"title",
+									"name"=>"Buy button options"
+								) ,
+								array(
+									"type"=>"select",
+									"name"=>"Show button icon",
+									"description"=>"Show icon on the cart icon on button.",
+									"id"=>"cwppos_show_icon",
+									"options"=>array("yes"=>"Yes","no"=>"No"),
+									"default"=>"yes"
+								),
+								array(	
+									"type"=>"color",
+									"name"=>"Button border",
+									"description"=>"Select the border color to be used on the buy button for the default state",
+									"id"=>"cwppos_buttonbd_color",
+									"default"=>"#3BAEDA"
+								),
+								array(	
+									"type"=>"color",
+									"name"=>"Button border hover",
+									"description"=>"Select the border color to be used on the buy button for the hover state",
+									"id"=>"cwppos_buttonbh_color",
+									"default"=>"#3BAEDA"
+								),
+								array(	
+									"type"=>"color",
+									"name"=>"Button background",
+									"description"=>"Select the background color to be used on the buy button for the default state",
+									"id"=>"cwppos_buttonbkd_color",
+									"default"=>"#ffffff"
+								),
+								array(	
+									"type"=>"color",
+									"name"=>"Button background hover",
+									"description"=>"Select the background color to be used on the buy button for the hover  state",
+									"id"=>"cwppos_buttonbkh_color",
+									"default"=>"#3BAEDA"
+								),
+								array(	
+									"type"=>"color",
+									"name"=>"Button text color",
+									"description"=>"Select the text color to be used on the buy button for the default state",
+									"id"=>"cwppos_buttontxtd_color",
+									"default"=>"#3BAEDA"
+								),
+								array(	
+									"type"=>"color",
+									"name"=>"Button text color hover",
+									"description"=>"Select the text color to be used on the buy button for the hover state",
+									"id"=>"cwppos_buttontxth_color",
+									"default"=>"#FFFFFF"
+								) 
+							
+							 )
+						) ,
+						array(
+							 "type"=>"tab",
+							 "name"=>"General settings",
+							 "options"=>array(
+								
+								array(
+									"type"=>"title",
+									"name"=>"General settings"
+								) , 
+								array(
+									"type"=>"select",
+									"name"=>"Show review comment",
+									"description"=>"Activate comment review user",
+									"id"=>"cwppos_show_userreview",
+									"options"=>array("yes"=>"Yes","no"=>"No"),
+									"default"=>"yes"
+								), 
+								array(
+									"type"=>"select",
+									"name"=>"Review position",
+									"description"=>"Position of the review box",
+									"id"=>"cwppos_show_reviewbox",
+									"options"=>array("yes"=>"After content","no"=>"Before content"),
+									"default"=>"yes"
+								)
+							
+							 )
+						) 
+
+		
+					);
+
+
+			 
+		}	 
+	
+	} 
--- latest/inc/cwp_top_products_widget.php
+++ latest/inc/cwp_top_products_widget.php
@@ -1,136 +1,136 @@
-<?php 
-/**
- * CWP - Top Producs Widget 
- */
-
-class cwp_top_products_widget extends WP_Widget {
-
-function __construct() {
-parent::__construct(
-'cwp_top_products_widget', 
-__('CWP Top Products Widget', 'cwppos'), 
-
-// Widget description
-array( 'description' => __( 'This widget displays the top products based on their rating.', 'cwppos' ), ) 
-);
-
-}
-
-	// Creating widget front-end
-	// This is where the action happens
-	public function widget( $args, $instance ) {
-		$title = apply_filters( 'widget_title', $instance['title'] );
-		$no_items = apply_filters( 'widget_content', $instance['no_items'] );
-		$cwp_tp_category = apply_filters( 'widget_content', $instance['cwp_tp_category'] );
-
-		// before and after widget arguments are defined by themes
-		echo "<div id='cwp_top_products_widget'>";
-		echo $args['before_widget'];
-		if ( ! empty( $title ) )
-		echo $args['before_title'] . $title . $args['after_title'];
-
-	// Loop to get the most popular posts, ordered by the author's final grade.
-		$query_args = array(
-			'posts_per_page'=> $no_items, // limit it to the specified no of posts
-			'post_type'		=> array('post'),
-			'review_category' => $cwp_tp_category, // limit it to the specified category
-				'meta_query'             => array(
-		array(
-			'key'       => 'cwp_meta_box_check',
-			'value'     => 'Yes',
-		),
-	),	
-		);
-
-		$cwp_top_products_loop = new WP_Query( $query_args );  
-		echo "<ul>";
-		while($cwp_top_products_loop->have_posts()) : $cwp_top_products_loop->the_post(); ?>
-
-		<li class="cwp-popular-review cwp_top_posts_widget_<?php the_ID(); ?>">
-		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-
-
-			<?php $review_score = get_post_meta($cwp_top_products_loop->post->ID, "option_overall_score", true);
-			if(!empty($review_score)) { ?>
-			<div class="review-grade">
-				<div class="chart">
-				<div class="percentage" data-percent="<?php echo $review_score; ?>"><span></span></div>
-				</div><!-- end .chart -->
-			</div>
-			<?php } ?>
-		</li><!-- end .popular-review -->
-
-		<?php endwhile; ?>
-		<?php wp_reset_postdata(); // reset the query 
-
-		echo "</ul>";
-		echo $args['after_widget'];
-		echo "</div>"; // end #cwp_top_products_widget
-	}
-
-	// Widget Backend 
-	public function form( $instance ) {
-		if ( isset( $instance[ 'title' ] ) ) {
-			$title = $instance[ 'title' ];
-			$no_items = $instance[ 'no_items' ];
-			$cwp_tp_category = $instance[ 'cwp_tp_category' ];
-		}
-		else {
-			$title = __( 'Top Products', 'cwppos' );
-			$no_items = __( '10', 'cwppos');
-			$cwp_tp_category = "Select Category";
-		}
-
-		$cwp_tp_categ_array = get_categories('hide_empty=0');
-		foreach ($cwp_tp_categ_array as $categs) {
-			$cwp_tp_all_categories[$categs->slug] = $categs->name;
-		}
-
-	// Widget admin form
-	?>
-	<p>
-	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', "cwppos" ); ?></label> 
-	<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-	</p>
-
-	<p>
-	<label for="<?php echo $this->get_field_id( 'no_items' ); ?>"><?php _e( 'Number of posts to show:', "cwppos" ); ?></label> 
-	<input id="<?php echo $this->get_field_id( 'no_items' ); ?>" name="<?php echo $this->get_field_name( 'no_items' ); ?>" size="3" type="text" value="<?php echo esc_attr( $no_items ); ?>" />
-	</p>
-
-	<p>
-	<?php $cwp_tp_selected_categ = esc_attr( $cwp_tp_category ); ?>
-	<label for="<?php echo $this->get_field_id( 'cwp_tp_category' ); ?>"><?php _e( 'Category:', "cwppos" ); ?></label> 
-	<select id="<?php echo $this->get_field_id( 'cwp_tp_category' ); ?>" name="<?php echo $this->get_field_name( 'cwp_tp_category' ); ?>">
-	<?php foreach ($cwp_tp_all_categories as $categ_slug => $categ_name): ?>
-			<?php if($categ_slug == $cwp_tp_selected_categ) {
-				echo "<option selected>".$categ_slug."</option>";
-			} elseif($categ_slug == "") {
-				echo "<option>There are no categs</select>";
-			} else { 
-				echo "<option>".$categ_slug."</option>";
-			} ?>
-	<?php endforeach; ?>
-	</select>
-	</p>
-
-	<?php }
-	
-	// Updating widget replacing old instances with new
-	public function update( $new_instance, $old_instance ) {
-		$instance = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-		$instance['no_items'] = ( ! empty( $new_instance['no_items'] ) ) ? strip_tags( $new_instance['no_items'] ) : '';
-		$instance['cwp_tp_category'] = ( ! empty( $new_instance['cwp_tp_category'] ) ) ? strip_tags( $new_instance['cwp_tp_category'] ) : '';
-
-		return $instance;
-	}
-
-} // end Class cwp_top_products_widget
-
-
-// Register and load the widget
-function cwp_load_top_products_widget() {
-	register_widget( 'cwp_top_products_widget' );
-}
+<?php 
+/**
+ * CWP - Top Producs Widget 
+ */
+
+class cwp_top_products_widget extends WP_Widget {
+
+function __construct() {
+parent::__construct(
+'cwp_top_products_widget', 
+__('CWP Top Products Widget', 'cwppos'), 
+
+// Widget description
+array( 'description' => __( 'This widget displays the top products based on their rating.', 'cwppos' ), ) 
+);
+
+}
+
+	// Creating widget front-end
+	// This is where the action happens
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		$no_items = apply_filters( 'widget_content', $instance['no_items'] );
+		$cwp_tp_category = apply_filters( 'widget_content', $instance['cwp_tp_category'] );
+
+		// before and after widget arguments are defined by themes
+		echo "<div id='cwp_top_products_widget'>";
+		echo $args['before_widget'];
+		if ( ! empty( $title ) )
+		echo $args['before_title'] . $title . $args['after_title'];
+
+	// Loop to get the most popular posts, ordered by the author's final grade.
+		$query_args = array(
+			'posts_per_page'=> $no_items, // limit it to the specified no of posts
+			'post_type'		=> array('post'),
+			'review_category' => $cwp_tp_category, // limit it to the specified category
+				'meta_query'             => array(
+		array(
+			'key'       => 'cwp_meta_box_check',
+			'value'     => 'Yes',
+		),
+	),	
+		);
+
+		$cwp_top_products_loop = new WP_Query( $query_args );  
+		echo "<ul>";
+		while($cwp_top_products_loop->have_posts()) : $cwp_top_products_loop->the_post(); ?>
+
+		<li class="cwp-popular-review cwp_top_posts_widget_<?php the_ID(); ?>">
+		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+
+
+			<?php $review_score = get_post_meta($cwp_top_products_loop->post->ID, "option_overall_score", true);
+			if(!empty($review_score)) { ?>
+			<div class="review-grade">
+				<div class="chart">
+				<div class="percentage" data-percent="<?php echo $review_score; ?>"><span></span></div>
+				</div><!-- end .chart -->
+			</div>
+			<?php } ?>
+		</li><!-- end .popular-review -->
+
+		<?php endwhile; ?>
+		<?php wp_reset_postdata(); // reset the query 
+
+		echo "</ul>";
+		echo $args['after_widget'];
+		echo "</div>"; // end #cwp_top_products_widget
+	}
+
+	// Widget Backend 
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+			$no_items = $instance[ 'no_items' ];
+			$cwp_tp_category = $instance[ 'cwp_tp_category' ];
+		}
+		else {
+			$title = __( 'Top Products', 'cwppos' );
+			$no_items = __( '10', 'cwppos');
+			$cwp_tp_category = "Select Category";
+		}
+
+		$cwp_tp_categ_array = get_categories('hide_empty=0');
+		foreach ($cwp_tp_categ_array as $categs) {
+			$cwp_tp_all_categories[$categs->slug] = $categs->name;
+		}
+
+	// Widget admin form
+	?>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', "cwppos" ); ?></label> 
+	<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+	</p>
+
+	<p>
+	<label for="<?php echo $this->get_field_id( 'no_items' ); ?>"><?php _e( 'Number of posts to show:', "cwppos" ); ?></label> 
+	<input id="<?php echo $this->get_field_id( 'no_items' ); ?>" name="<?php echo $this->get_field_name( 'no_items' ); ?>" size="3" type="text" value="<?php echo esc_attr( $no_items ); ?>" />
+	</p>
+
+	<p>
+	<?php $cwp_tp_selected_categ = esc_attr( $cwp_tp_category ); ?>
+	<label for="<?php echo $this->get_field_id( 'cwp_tp_category' ); ?>"><?php _e( 'Category:', "cwppos" ); ?></label> 
+	<select id="<?php echo $this->get_field_id( 'cwp_tp_category' ); ?>" name="<?php echo $this->get_field_name( 'cwp_tp_category' ); ?>">
+	<?php foreach ($cwp_tp_all_categories as $categ_slug => $categ_name): ?>
+			<?php if($categ_slug == $cwp_tp_selected_categ) {
+				echo "<option selected>".$categ_slug."</option>";
+			} elseif($categ_slug == "") {
+				echo "<option>There are no categs</select>";
+			} else { 
+				echo "<option>".$categ_slug."</option>";
+			} ?>
+	<?php endforeach; ?>
+	</select>
+	</p>
+
+	<?php }
+	
+	// Updating widget replacing old instances with new
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['no_items'] = ( ! empty( $new_instance['no_items'] ) ) ? strip_tags( $new_instance['no_items'] ) : '';
+		$instance['cwp_tp_category'] = ( ! empty( $new_instance['cwp_tp_category'] ) ) ? strip_tags( $new_instance['cwp_tp_category'] ) : '';
+
+		return $instance;
+	}
+
+} // end Class cwp_top_products_widget
+
+
+// Register and load the widget
+function cwp_load_top_products_widget() {
+	register_widget( 'cwp_top_products_widget' );
+}
 add_action( 'widgets_init', 'cwp_load_top_products_widget' );
