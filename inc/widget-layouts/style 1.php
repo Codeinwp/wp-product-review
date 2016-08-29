7d8b72e6477554312465da48226fbf68b15ee14a
contactashish13 <contactashish13@gmail.com>
2016-08-29 09:35:45 +0530

    add filters for nofollow links #116

--- inc/widget-layouts/style 1.php
+++ inc/widget-layouts/style 1.php
@@ -54,12 +54,17 @@
                         }
                         ?>
                     <p class="wppr-style1-buttons">
-                        <?php if (!empty($instance['cwp_tp_buynow'])) { ?>
-                            <a href='<?php echo $affiliate_link;?>' rel='nofollow' target='_blank' class='wppr-bttn'><?php _e($instance['cwp_tp_buynow'], "cwppos");?></a> 
-                        <?php } ?>
-                        <?php if (!empty($instance['cwp_tp_readreview'])) { ?>
-                            <a href='<?php echo $review_link;?>' rel='nofollow' class='wppr-bttn'><?php _e($instance['cwp_tp_readreview'], "cwppos");?></a> 
-                        <?php } ?>
+                        <?php
+                            $link   = "<a href='{$affiliate_link}' rel='nofollow' target='_blank' class='wppr-bttn'>" . __($instance['cwp_tp_buynow'], "cwppos") . "</a>";
+                            if (!empty($instance['cwp_tp_buynow'])) {
+                                echo apply_filters("wppr_widget_style1_buynow_link", $link, get_the_ID(), $affiliate_link, $instance['cwp_tp_buynow']);
+                            }
+
+                            $link   = "<a href='{$review_link}' rel='nofollow' target='_blank' class='wppr-bttn'>" . __($instance['cwp_tp_readreview'], "cwppos") . "</a>";
+                            if (!empty($instance['cwp_tp_readreview'])) {
+                                echo apply_filters("wppr_widget_style1_readreview_link", $link, get_the_ID(), $review_link, $instance['cwp_tp_readreview']);
+                            }
+                        ?>
                     </p>
                 </div>
                 <div class="clear"></div>
