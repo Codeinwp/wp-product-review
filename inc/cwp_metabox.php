6964e8009fadbaad14553eea3eeb3efa94ede48f
contactashish13 <contactashish13@gmail.com>
2016-09-20 19:11:32 +0530

    Revert "shortcode alg #39"
    This reverts commit d309b33c525ffc0db358959c818439bb787fd12d.

--- inc/cwp_metabox.php
+++ inc/cwp_metabox.php
@@ -287,15 +287,15 @@ function cwp_review_meta_boxes_save($post_id){
     if( isset( $_POST[ 'cwp_meta_box_check' ] ) && $_POST[ 'cwp_meta_box_check' ]=="Yes") {
 
     if( isset( $_POST[ 'cwp_rev_product_name' ] ) ) {
-        update_post_meta( $post_id, 'cwp_rev_product_name', apply_filters('wppr_sanitize_product_title', $_POST[ 'cwp_rev_product_name' ]) );
+        update_post_meta( $post_id, 'cwp_rev_product_name', sanitize_text_field( $_POST[ 'cwp_rev_product_name' ] ) );
     }
 
     if( isset( $_POST[ 'cwp_rev_price' ] ) ) {
-        update_post_meta( $post_id, 'cwp_rev_price', apply_filters('wppr_sanitize_product_price', $_POST[ 'cwp_rev_price' ]) );
+        update_post_meta( $post_id, 'cwp_rev_price', sanitize_text_field( $_POST[ 'cwp_rev_price' ] ) );
     }
 
     if( isset( $_POST[ 'cwp_image_link' ] ) ) {
-        update_post_meta( $post_id, 'cwp_image_link', apply_filters('wppr_sanitize_product_image', $_POST[ 'cwp_image_link' ]) );
+        update_post_meta( $post_id, 'cwp_image_link', sanitize_text_field( $_POST[ 'cwp_image_link' ] ) );
     }
 
 
@@ -305,7 +305,7 @@ function cwp_review_meta_boxes_save($post_id){
 
 
     if( isset( $_POST[ 'cwp_product_affiliate_link' ] ) ) {
-        update_post_meta( $post_id, 'cwp_product_affiliate_link', apply_filters('wppr_sanitize_link1', $_POST[ 'cwp_product_affiliate_link' ]) );
+        update_post_meta( $post_id, 'cwp_product_affiliate_link', esc_url( $_POST[ 'cwp_product_affiliate_link' ] ) );
     }
 
         if( isset( $_POST[ 'cwp_product_affiliate_text2' ] ) ) {
@@ -314,7 +314,7 @@ function cwp_review_meta_boxes_save($post_id){
 
 
     if( isset( $_POST[ 'cwp_product_affiliate_link2' ] ) ) {
-        update_post_meta( $post_id, 'cwp_product_affiliate_link2', apply_filters('wppr_sanitize_link2', $_POST[ 'cwp_product_affiliate_link2' ]) );
+        update_post_meta( $post_id, 'cwp_product_affiliate_link2', esc_url( $_POST[ 'cwp_product_affiliate_link2' ] ) );
     }
 
     if(  !empty($_POST[ 'cwp_bar_icon' ] )) {
