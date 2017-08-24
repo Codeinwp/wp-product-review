4be0b4fc7921a119ed9220b7423d039ba4010764
Marius Cristea <marius.cristea@vertistudio.com>
2017-08-25 02:23:36 +0300

    Fixed title of the review using a empty id.

--- includes/public/layouts/widget/default.php
+++ includes/public/layouts/widget/default.php
@@ -11,14 +11,14 @@
 
 echo '<ul>';
 foreach ( $results as $review ) :
-	$product_title_display = ( $instance['post_type'] == true ) ? $review['cwp_rev_product_name'] : get_the_title();
+	$product_title_display = ( $instance['post_type'] == true ) ? $review['cwp_rev_product_name'] : get_the_title($review['ID']);
 	$product_image = $review['cwp_rev_product_image'];
 
 	if ( strlen( $product_title_display ) > self::RESTRICT_TITLE_CHARS ) {
 		$product_title_display = substr( $product_title_display, 0, self::RESTRICT_TITLE_CHARS ) . '...';
 	}
 	?>
-	<li class="cwp-popular-review cwp_top_posts_widget_
+    <li class="cwp-popular-review cwp_top_posts_widget_
 	<?php
 	if ( $instance['show_image'] == true && ! empty( $product_image ) ) {
 		echo ' wppr-cols-3';
@@ -30,24 +30,24 @@ foreach ( $results as $review ) :
 		<?php
 		if ( $instance['show_image'] == true && ! empty( $product_image ) ) {
 			?>
-			<img class="cwp_rev_image wppr-col" src="<?php echo $product_image; ?>"
-				 alt="<?php echo $review['cwp_rev_product_name']; ?>">
+            <img class="cwp_rev_image wppr-col" src="<?php echo $product_image; ?>"
+                 alt="<?php echo $review['cwp_rev_product_name']; ?>">
 		<?php } ?>
-		<a href="<?php echo get_the_permalink( $review['ID'] ); ?>" class="wppr-col"
-		   title="<?php echo $review['cwp_rev_product_name']; ?>">
+        <a href="<?php echo get_the_permalink( $review['ID'] ); ?>" class="wppr-col"
+           title="<?php echo $review['cwp_rev_product_name']; ?>">
 			<?php echo $product_title_display; ?>
-		</a>
+        </a>
 		<?php
 		$review_score = $review['wppr_rating'];
 		if ( ! empty( $review_score ) ) {
 			?>
-			<div class="review-grade-widget wppr-col">
-				<div class="cwp-review-chart absolute">
-					<div class="cwp-review-percentage" data-percent="<?php echo $review_score; ?>"><span></span></div>
-				</div><!-- end .chart -->
-			</div>
+            <div class="review-grade-widget wppr-col">
+                <div class="cwp-review-chart absolute">
+                    <div class="cwp-review-percentage" data-percent="<?php echo $review_score; ?>"><span></span></div>
+                </div><!-- end .chart -->
+            </div>
 		<?php } ?>
-	</li>
+    </li>
 	<?php
 endforeach;
 ?>
