cc8816d5a40460dbf4d069b2e2add1b36bf31cfd
selu91 <cristea.marius.alexandru@gmail.com>
2017-09-06 14:27:27 +0300

    Added global variable in editor layout.

--- includes/admin/layouts/editor-default-tpl.php
+++ includes/admin/layouts/editor-default-tpl.php
@@ -4,6 +4,7 @@
  *
  * @package     WPPR
  * @subpackage  Layouts
+ * @global      $model WPPR_Editor_Model
  * @copyright   Copyright (c) 2017, Bogdan Preda
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       3.0.0
