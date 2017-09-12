bb1e9ce927f3c8a1f9471e139a3d741c03cd40a3
selu91 <cristea.marius.alexandru@gmail.com>
2017-09-12 16:23:32 +0300

    Added depracted tags.

--- includes/public/layouts/score-tpl.php
+++ includes/public/layouts/score-tpl.php
@@ -7,6 +7,7 @@
  * @copyright   Copyright (c) 2017, Bogdan Preda
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       3.0.0
+ * @deprecated
  */
 
 $output = number_format( floor( $review_object->get_rating() ) / 10, 1 );
