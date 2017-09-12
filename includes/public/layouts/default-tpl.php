fd40c7682cdd7bdb76210287fc496b3283ffd023
selu91 <cristea.marius.alexandru@gmail.com>
2017-09-12 17:08:31 +0300

    Fix for output.

--- includes/public/layouts/default-tpl.php
+++ includes/public/layouts/default-tpl.php
@@ -4,7 +4,12 @@
  *
  * @deprecated
  * @package WPPR
- * Legacy template for backwards compat.
  */
+ob_start();
 include_once WPPR_PATH . '/includes/public/layouts/default.php';
+if ( ! isset( $output ) ) {
+	$output = '';
+}
+$output .= ob_get_contents();
 
+ob_end_clean();
