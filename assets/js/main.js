90f7755d48d29b4908934dd1973acbe035227d82
selu91 <cristea.marius.alexandru@gmail.com>
2017-09-18 16:46:34 +0300

    Added box width custmization option.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,10 @@
 /**
  * Main JavaScript File
  */
-jQuery( document ).ready(function($) {
+jQuery(document).ready(function ($) {
 
-	var initPieChart = function() {
-		returnColor = function(percent) {
+	var initPieChart = function () {
+		returnColor = function (percent) {
 			if (percent > 0 && percent <= 25) {
 				return c1;
 			} else if (percent > 25 && percent <= 50) {
@@ -22,25 +22,36 @@ jQuery( document ).ready(function($) {
 			trackColorRight = '#ebebeb';
 		}
 
-		p_image_width = $( '.review-wrap-up .rev-wu-image' ).width();
-		if (p_image_width == 0 || p_image_width == null) {   p_image_width = 50;
+		var box_width = $('.cwp-review-percentage').attr('data-box-size');
+
+		box_width = parseInt(box_width);
+		if (isNaN(box_width)) {
+
+			var p_image_width = $('.review-wrap-up .rev-wu-image').width();
+			if (p_image_width == 0 || p_image_width == null) {
+				p_image_width = 50;
+			}
+			box_width = p_image_width;
+			box_width = Math.min(box_width, 150) * 0.8;
+
 		}
-		$( '.cwp-review-percentage' ).cwp_easyPieChart({
-			barColor: function(percent) {
-				return returnColor( percent );
+		var line_width = 15 * Math.min(box_width, 150) / 140;
+		$('.cwp-review-percentage').cwp_easyPieChart({
+			barColor: function (percent) {
+				return returnColor(percent);
 			},
 			trackColor: trackColorRight,
 			scaleColor: false,
 			lineCap: 'butt',
 			rotate: 0,
-			lineWidth: 15 * Math.min( p_image_width,150 ) / 140,
+			lineWidth: line_width,
 			animate: 1,
-			size : Math.min( p_image_width,150 ) * 0.8,
-			onStep: function(value) {
-				var c = returnColor( value );
+			size: box_width,
+			onStep: function (value) {
+				var c = returnColor(value);
 
-				this.$el.find( 'span' ).text( ~~value / 10 );
-				this.$el.find( 'span' ).css({
+				this.$el.find('span').text(~~value / 10);
+				this.$el.find('span').css({
 					color: c
 				});
 			}
@@ -51,50 +62,50 @@ jQuery( document ).ready(function($) {
 
 	function wuReview() {
 
-		if ( ! (typeof(cwpCustomBarIcon) === "undefined") && ! (cwpCustomBarIcon === "") && isSetToPro) {
-			$( ".rev-option" ).each(function() {
-	            var grade = $( this ).attr( "data-value" );
-	            $( this ).addClass( "customBarIcon" );
-	            var x = 10;
-	            if ($( this ).children( "ul" ).find( "li" ).length == 0) {
+		if (!(typeof(cwpCustomBarIcon) === "undefined") && !(cwpCustomBarIcon === "") && isSetToPro) {
+			$(".rev-option").each(function () {
+				var grade = $(this).attr("data-value");
+				$(this).addClass("customBarIcon");
+				var x = 10;
+				if ($(this).children("ul").find("li").length == 0) {
 					for (var i = 0; i < x; i++) {
-						$( this ).children( "ul" ).append( "<li><i class='fa fa-fw'>&" + cwpCustomBarIcon + "</i></li>" );
+						$(this).children("ul").append("<li><i class='fa fa-fw'>&" + cwpCustomBarIcon + "</i></li>");
 					}
 				}
-	            $( this ).children( "ul" ).children( "li:nth-child(-n+" + Math.ceil( grade / 10 ) + ")" ).css( "color", returnColor( grade ) );
-	            $( this ).children( "div" ).children( "span" ).text( grade / 10 + "/10" );
+				$(this).children("ul").children("li:nth-child(-n+" + Math.ceil(grade / 10) + ")").css("color", returnColor(grade));
+				$(this).children("div").children("span").text(grade / 10 + "/10");
 			});
 		} else {
-			$( ".rev-option" ).each(function() {
-	            var grade = $( this ).attr( "data-value" );
-	            var x = 10;
-	            if ($( this ).children( "ul" ).find( "li" ).length == 0) {
+			$(".rev-option").each(function () {
+				var grade = $(this).attr("data-value");
+				var x = 10;
+				if ($(this).children("ul").find("li").length == 0) {
 					for (var i = 0; i < x; i++) {
-						$( this ).children( "ul" ).append( "<li style='margin-right: 2%;'></li>" );
+						$(this).children("ul").append("<li style='margin-right: 2%;'></li>");
 					}
 				}
-	            $( this ).children( "ul" ).children( "li:nth-child(-n+" + Math.ceil( grade / 10 ) + ")" ).css( "background", returnColor( grade ) );
-	            $( this ).children( "div" ).children( "span" ).text( grade / 10 + "/10" );
-	        });
+				$(this).children("ul").children("li:nth-child(-n+" + Math.ceil(grade / 10) + ")").css("background", returnColor(grade));
+				$(this).children("div").children("span").text(grade / 10 + "/10");
+			});
 		}
 
 	}
 
-	$( ".comment-meta-option .comment-meta-grade" ).each(function() {
-		var theBarWidth = (100 * parseFloat( $( this ).css( 'width' ) ) / parseFloat( $( this ).parent().css( 'width' ) ));
-		$( this ).css( "background", returnColor( theBarWidth ) );
+	$(".comment-meta-option .comment-meta-grade").each(function () {
+		var theBarWidth = (100 * parseFloat($(this).css('width')) / parseFloat($(this).parent().css('width')));
+		$(this).css("background", returnColor(theBarWidth));
 	});
 
 	wuReview();
 
-	$( ".wppr-comment-meta-slider" ).each(function() {
-		var comm_meta_input = $( this ).parent( ".wppr-comment-form-meta" ).children( "input" );
-		$( this ).slider({
+	$(".wppr-comment-meta-slider").each(function () {
+		var comm_meta_input = $(this).parent(".wppr-comment-form-meta").children("input");
+		$(this).slider({
 			min: 0,
 			max: 100,
 			value: 4,
-			slide: function(event, ui) {
-				$( comm_meta_input ).val( ui.value / 10 );
+			slide: function (event, ui) {
+				$(comm_meta_input).val(ui.value / 10);
 			}
 		});
 	});
