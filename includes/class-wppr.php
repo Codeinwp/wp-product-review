68d9e8ed22290585a44669aa6e46fa2498808dc8
selu91 <cristea.marius.alexandru@gmail.com>
2018-02-21 11:36:05 +0200

    Bump version.

--- includes/class-wppr.php
+++ includes/class-wppr.php
@@ -67,7 +67,7 @@ class WPPR {
 	 */
 	public function __construct() {
 		$this->plugin_name = 'wppr';
-		$this->version     = '3.4.0';
+		$this->version     = '3.4.1';
 
 		$this->load_dependencies();
 		$this->set_locale();
