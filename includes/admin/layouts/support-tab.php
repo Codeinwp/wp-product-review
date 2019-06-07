7cb7a61955661426858564371b9263088e7e8780
Ashish Ravi <contactashish13@gmail.com>
2019-06-07 11:28:02 +0530

    grunt

--- includes/admin/layouts/support-tab.php
+++ includes/admin/layouts/support-tab.php
@@ -2,7 +2,7 @@
 		<div class="pro-feature-inner">
 			<div class="pro-feature-features">
 				<h2>Welcome to WP Product Review</h2>
-				<p>WPPR makes it easy to create review posts in WordPress. We recommend watching this Getting started video, and then try the plugin for yourself by enabling the review post option in the settings menu - start configuring the plugin <a href="<?php echo admin_url('/admin.php?page=wppr');?>">here</a></p>
+				<p>WPPR makes it easy to create review posts in WordPress. We recommend watching this Getting started video, and then try the plugin for yourself by enabling the review post option in the settings menu - start configuring the plugin <a href="<?php echo admin_url( '/admin.php?page=wppr' ); ?>">here</a></p>
 				<p>You can also read our guide on how to set up and create your first review. Read our documentation <a href="https://docs.themeisle.com/category/754-wp-product-review" target="_new">here</a>.</p>
 			</div>
 			<div class="pro-feature-image">
