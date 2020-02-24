730880eb46bc54514c58f012fbadea1d6fedcef4
Rodica-Elena Irodiu <rodica.andronache@vertistudio.com>
2020-02-24 13:22:27 +0200

    chore: version bump

--- includes/class-wppr.php
+++ includes/class-wppr.php
@@ -67,7 +67,7 @@ class WPPR {
 	 */
 	public function __construct() {
 		$this->plugin_name = 'wppr';
-		$this->version     = '3.7.2';
+		$this->version     = '3.7.3';
 
 		$this->load_dependencies();
 		$this->set_locale();
