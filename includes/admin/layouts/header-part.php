cc3a08e55002e37fc9cf7d5a3a3c8c226c54b1f9
Ashish Ravi <contactashish13@gmail.com>
2020-02-24 15:18:27 +0530

    remove pro mentions in the free library

--- includes/admin/layouts/header-part.php
+++ includes/admin/layouts/header-part.php
@@ -14,11 +14,5 @@
 	<p class="logo">WP Product Review</p>
 	<span class="slogan">by <a
 			href="https://themeisle.com/">ThemeIsle</a></span>
-	<div class="header-btns">
-		<?php if ( ! class_exists( 'WPPR_Pro' ) ) { ?>
-		<a target="_blank" href="<?php echo WPPR_UPSELL_LINK; ?>" class="buy-now"><span
-				class="dashicons dashicons-cart"></span> More features</a>
-		<?php } ?>
-	</div>
 	<div class="clear"></div>
 </div>
